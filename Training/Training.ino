c711f6bc0f04871a8cd308a7cd1ed2e2858a0e22
Rares Moldovan <rares.moldo997@gmail.com>
2022-10-03 17:01:08 +0300

    COntrolling two LEDs with two buttons

--- Training/Training.ino
+++ Training/Training.ino
@@ -1,9 +1,44 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+*  The purpose of this project is to turn off or on three LEDs using for each of them a button.
+*  Equipment used: - Board Jade Mega
+*                  - Two buttons with 10k Ohm resistors
+*                  - Three LEDs with 220 Ohm resistors
+*                  - Breadboard
+*/
 
+#define RED_LED 26
+#define BLUE_LED 27
+#define GREEN_LED 28
+
+#define BUTTON_RED_LED 34
+#define BUTTON_BLUE_LED 35
+
+void setup() {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BUTTON_RED_LED, INPUT);
+  pinMode(BUTTON_BLUE_LED, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(BUTTON_RED_LED) == HIGH){
+    digitalWrite(RED_LED, HIGH);
+  }
+  else{
+    digitalWrite(RED_LED, LOW);
+  }
+
+  if(digitalRead(BUTTON_BLUE_LED) == HIGH){
+    digitalWrite(BLUE_LED, HIGH);
+  }
+  else{
+    digitalWrite(BLUE_LED, LOW);
+  }
 
 }
+
+
+
+
+
