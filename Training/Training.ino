13ee3d85f47318c5579abebc96d20f63dbbc5564
Rares Moldovan <rares.moldo997@gmail.com>
2022-10-03 20:41:25 +0300

    RGB LEDs controlled

--- Training/Training.ino
+++ Training/Training.ino
@@ -1,44 +1,52 @@
 /*
-*  The purpose of this project is to turn off or on three LEDs using for each of them a button.
+*  Use an KY-016 AND KY-009 RGB color module.
 *  Equipment used: - Board Jade Mega
-*                  - Two buttons with 10k Ohm resistors
-*                  - Three LEDs with 220 Ohm resistors
+*                  - One LED with 220 Ohm resistors
 *                  - Breadboard
 */
 
-#define RED_LED 26
-#define BLUE_LED 27
-#define GREEN_LED 28
+#define RED_LED A0
+#define GREEN_LED A1
+#define BLUE_LED A2
 
-#define BUTTON_RED_LED 34
-#define BUTTON_BLUE_LED 35
+#define RED_LED_KY A3
+#define GREEN_LED_KY A4
+#define BLUE_LED_KY A5
 
-void setup() {
+long randNumberRed = 0;
+long randNumberGreen = 0;
+long randNumberBlue = 0;
+
+void setup(){
   pinMode(RED_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
-  pinMode(BUTTON_RED_LED, INPUT);
-  pinMode(BUTTON_BLUE_LED, INPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  randomSeed(analogRead(0));
 }
 
-void loop() {
-  if(digitalRead(BUTTON_RED_LED) == HIGH){
-    digitalWrite(RED_LED, HIGH);
-  }
-  else{
-    digitalWrite(RED_LED, LOW);
-  }
+void loop(){
+  randNumberRed = random(0, 255);
+  randNumberGreen = random(0, 255);
+  randNumberBlue = random(0, 255);
+  change_color(randNumberRed, randNumberGreen, randNumberBlue);
+  delay(500);
+  
+  randNumberRed = random(0, 255);
+  randNumberGreen = random(0, 255);
+  randNumberBlue = random(0, 255);
+  change_color_KY(randNumberRed, randNumberGreen, randNumberBlue);
+  delay(500);
+}
 
-  if(digitalRead(BUTTON_BLUE_LED) == HIGH){
-    digitalWrite(BLUE_LED, HIGH);
-  }
-  else{
-    digitalWrite(BLUE_LED, LOW);
-  }
-
+void change_color_KY(int redComponent, int greenComponent, int blueComponent){
+  analogWrite(RED_LED_KY, redComponent);
+  analogWrite(GREEN_LED_KY, greenComponent);
+  analogWrite(BLUE_LED_KY, blueComponent);
 }
 
 
-
-
-
+void change_color(int redComponent, int greenComponent, int blueComponent){
+  analogWrite(RED_LED, redComponent);
+  analogWrite(GREEN_LED, greenComponent);
+  analogWrite(BLUE_LED, blueComponent);
+}
