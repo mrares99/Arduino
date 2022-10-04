1cf985a36d1e567d81f6854cdbbf724dfe971ec4
Rares Moldovan <rares.moldo997@gmail.com>
2022-10-04 20:17:07 +0300

    Two buzzers used simultaneously

--- Training/Training.ino
+++ Training/Training.ino
@@ -1,52 +1,26 @@
 /*
-*  Use an KY-016 AND KY-009 RGB color module.
+*  Two buzzers are used at the same time.
 *  Equipment used: - Board Jade Mega
-*                  - One LED with 220 Ohm resistors
+*                  - Two buzzers
 *                  - Breadboard
 */
-
-#define RED_LED A0
-#define GREEN_LED A1
-#define BLUE_LED A2
-
-#define RED_LED_KY A3
-#define GREEN_LED_KY A4
-#define BLUE_LED_KY A5
-
-long randNumberRed = 0;
-long randNumberGreen = 0;
-long randNumberBlue = 0;
-
-void setup(){
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(BLUE_LED, OUTPUT);
-  randomSeed(analogRead(0));
-}
-
-void loop(){
-  randNumberRed = random(0, 255);
-  randNumberGreen = random(0, 255);
-  randNumberBlue = random(0, 255);
-  change_color(randNumberRed, randNumberGreen, randNumberBlue);
-  delay(500);
+#define BUZZER1 36
+#define BUZZER2 37
   
-  randNumberRed = random(0, 255);
-  randNumberGreen = random(0, 255);
-  randNumberBlue = random(0, 255);
-  change_color_KY(randNumberRed, randNumberGreen, randNumberBlue);
-  delay(500);
+void setup ()
+{
+ pinMode(BUZZER1, OUTPUT);
+ pinMode(BUZZER2, OUTPUT);
 }
-
-void change_color_KY(int redComponent, int greenComponent, int blueComponent){
-  analogWrite(RED_LED_KY, redComponent);
-  analogWrite(GREEN_LED_KY, greenComponent);
-  analogWrite(BLUE_LED_KY, blueComponent);
-}
-
-
-void change_color(int redComponent, int greenComponent, int blueComponent){
-  analogWrite(RED_LED, redComponent);
-  analogWrite(GREEN_LED, greenComponent);
-  analogWrite(BLUE_LED, blueComponent);
+  
+void loop ()
+{
+  digitalWrite(BUZZER1, HIGH);
+  delay(100);
+  digitalWrite(BUZZER1, LOW);
+  delay(100);
+  digitalWrite(BUZZER2, HIGH);
+  delay(100);
+  digitalWrite(BUZZER2, LOW);
+  delay(100);
 }
